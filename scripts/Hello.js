d1bb50d3b11d5f5570daac53d6d0a1ef6d0a0ebe
amysimmons <amy__simmons@hotmail.com>
2016-02-08 17:26:19 +1100

    first commit react from scratch

---
+++ scripts/Hello.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Hello = React.createClass({
+  render(){
+    return(
+      <div>Hello World! My name is Amy.</div>
+    );
+  }
+});
+
+module.exports = Hello;
