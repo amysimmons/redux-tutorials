6f64e88574ffb5b6653e5cef6bb4b6bdfb9560e5
amysimmons <amy__simmons@hotmail.com>
2016-02-16 21:45:44 +1100

    react reduct counter example

--- scripts/Counter.js
+++ scripts/Counter.js
@@ -1,6 +1,8 @@
 const React = require('react');
+const ReactDom = require('react-dom');
 const Redux = require('redux');
 
+//the reducer 
 const counter = (state = 0, action) => {
 	switch (action.type){
 		case 'INCREMENT':
@@ -12,6 +14,20 @@ const counter = (state = 0, action) => {
 	}
 }
 
+//dumb Counter component, containing no business logic, 
+//it just specified how the state is rendered and how the callbacks are bound to the events
+const Counter = ({
+	value,
+	onIncrement,
+	onDecrement
+}) => (
+	<div>
+		<h1>{value}</h1>
+		<button onClick={onIncrement}>+</button>
+		<button onClick={onDecrement}>-</button>
+	</div>
+);
+
 const {createStore} = Redux;
 //the above line is es6, it is the same as:
 //var createStore = Redux.createStore;
@@ -22,13 +38,23 @@ const store = createStore(counter);
 //the above line calls createStore with counter as the reducer that manages the state updates
 
 const render = () => {
-	document.body.innerText = store.getState();
-}
+	ReactDom.render(
+		<Counter 
+			value={store.getState()}
+			onIncrement={()=>
+				store.dispatch({
+					type: 'INCREMENT'
+				})
+			}
+			onDecrement={()=>
+				store.dispatch({
+					type: 'DECREMENT'
+				})
+			}/>, 
+		document.getElementById('counter')
+	);
+};
 
+//we subscribe to the redux store so our render function runs any time the state changes
 store.subscribe(render);
-
 render();
-
-document.addEventListener('click', () => {
-	store.dispatch({type: 'INCREMENT'});
-});
--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,12 @@ const React = require('react');
 const ReactDom = require('react-dom');
 const Counter = require('./Counter');
 
-ReactDom.render(<Counter />, document.querySelector('#container'));
+
+
+//the render function is called any time the store state changes
+//so i can pass the current state of the store as a prop to my root component
+
+ReactDom.render(
+	<Counter/>, 
+	document.querySelector('#container')
+);
